a91d65eff317f87bae95ee4b78202bd5e0a9348f
Jordan (JDude77) <jordy@oaksydix.com>
2019-12-12 02:35:34 +0000

    Upload 2
    Not made too much progress, but started figuring out how to utilise and create JSON files, and have appropriate scripts and files holding working code for that purpose.

---
+++ Graded Unit Game/Assets/NewBehaviourScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [System.Serializable]
+    public struct TheMainThing
+    {
+        public List<MyJsonData> entries;
+    }
+
+    [System.Serializable]
+    public struct MyJsonData
+    {
+        public int theNumber;
+        public string theString;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Load text from a JSON file (Assets/Resources/Text/myData.json)
+        TextAsset jsonTextFile = Resources.Load<TextAsset>("JSON/dialog");
+
+        //Then use JsonUtility.FromJson<T>() to deserialize jsonTextFile into an object
+        TheMainThing newData = JsonUtility.FromJson<TheMainThing>(jsonTextFile.text);
+        Debug.Log("Count = " + newData.entries.Count);
+        foreach (MyJsonData item in newData.entries)
+        {
+            Debug.Log("Item = {" + item.theString + ", " + item.theNumber + "}");
+        }
+        Debug.Log("Hi");
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Graded Unit Game/Assets/Scripts/Game/GameState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    private void Awake()
+    {
+        //Make sure there is only one game manager
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Game Manager");
+        if(objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }//End if
+
+        //Don't destroy the state tracker between 
+        DontDestroyOnLoad(this.gameObject);
+    }//End awake
+}
